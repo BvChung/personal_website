32e67c075e5e8b6fbcf810209698068d477eea10
Brandon Chung <chung.brandon123@gmail.com>
2024-01-07 23:50:40 -0600

    Update

--- astro.config.mjs
+++ astro.config.mjs
@@ -2,16 +2,20 @@ import { defineConfig } from "astro/config";
 import mdx from "@astrojs/mdx";
 import tailwind from "@astrojs/tailwind";
 import sitemap from "@astrojs/sitemap";
+import remarkMath from "remark-math";
+import rehypeKatex from "rehype-katex";
 
 // https://astro.build/config
 export default defineConfig({
-  integrations: [mdx(), tailwind(), sitemap()],
-  site: "https://demo.maxencewolff.com/",
-  markdown: {
-    syntaxHighlight: "shiki",
-    shikiConfig: {
-      theme: "dracula-soft",
-      wrap: true,
-    },
-  },
+	integrations: [mdx(), tailwind(), sitemap()],
+	site: "https://demo.maxencewolff.com/",
+	markdown: {
+		syntaxHighlight: "shiki",
+		shikiConfig: {
+			theme: "dracula-soft",
+			wrap: true,
+		},
+		remarkPlugins: [remarkMath],
+		rehypePlugins: [rehypeKatex],
+	},
 });
