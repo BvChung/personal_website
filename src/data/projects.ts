288d8690f107a22404a1589d1b7ed8f9467d789a
Brandon Chung <chung.brandon123@gmail.com>
2024-01-16 20:39:09 -0600

    Update

--- src/data/projects.ts
+++ src/data/projects.ts
@@ -7,18 +7,18 @@ export type Project = {
 
 export const projects: Project[] = [
 	{
-		title: "Covid-19 Machine Learning Model",
+		title: "Covid-19 Machine Learning Models",
 		tech: ["Python", "SciKit-Learn", "Pandas", "NumPy"],
 		link: "https://github.com/BvChung/covid-machine-learning-analysis",
 		description:
-			"Machine learning regression models to predict the number of ICU admissions with US COVID-19 dataset.",
+			"Machine learning models to predict the number of ICU admissions with US COVID-19 dataset.",
 	},
 	{
 		title: "CougarCS Admin Portal",
 		tech: ["NextJS", "TypeScript", "Tailwind CSS, PostgreSQL"],
 		link: "https://github.com/CougarCS/CougarCS-AdminPortal",
 		description:
-			"An administrative dashboard designed for club officers, streamlining the management of members and events.",
+			"An administrative dashboard designed for club officers, streamlining member and event management.",
 	},
 	{
 		title: "Personal Website",
